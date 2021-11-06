af15e0fd0ee9a9f41106e192652116cae698b2a1
ajdixon30 <adam.dixon30@gmail.com>
2021-11-06 19:52:06 -0400

    Finished controller class allowing new movies to be saved to the database

--- src/main/java/com/Revature/Project2/beans/controllers/MovieController.java
+++ src/main/java/com/Revature/Project2/beans/controllers/MovieController.java
@@ -1,22 +1,28 @@
 package com.Revature.Project2.beans.controllers;
 
+import com.Revature.Project2.beans.pojos.Movie;
 import com.Revature.Project2.services.DisplayMovies;
+import com.Revature.Project2.services.GetMovies;
+import com.Revature.Project2.services.ManageMovies;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/movie")
 public class MovieController {
     private final DisplayMovies display;
+    private final GetMovies get;
+    private final ManageMovies manage;
 
     @Autowired
-    public MovieController(DisplayMovies display) {
+    public MovieController(DisplayMovies display, GetMovies get, ManageMovies manage) {
         this.display = display;
+        this.get = get;
+        this.manage = manage;
     }
 
 //    @GetMapping(value = "/all-movies", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -24,4 +30,19 @@ public class MovieController {
 //    //public String get() {
         //return display.displayAllMovies();
     //}
+    @PostMapping(value = "/newMovie", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Movie newMovie(@RequestParam String title){
+        Movie movie = new Movie();
+        List<String> props;
+        String id = get.getImdbId(title);
+        props = get.getMovieById(id);
+        movie.setTitle(props.get(0));
+        movie.setGenre(props.get(1));
+        try {
+            manage.saveMovie(movie);//Saves the newly created movie object
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        return movie;
+    }
 }
