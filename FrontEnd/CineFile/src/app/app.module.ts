c0013df41add297bcf2bd31c4951971c2df0eb6b
BrittLowell <brittany.lowell@revature.net>
2021-11-09 09:08:22 -0700

    added admin and user folders and added more components

--- FrontEnd/CineFile/src/app/app.module.ts
+++ FrontEnd/CineFile/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerHomePageComponent } from './customer-home-page/customer-home-page.component';
-import { AdminHomePageComponent } from './admin-home-page/admin-home-page.component';
+import { CustomerHomePageComponent } from './User/customer-home-page/customer-home-page.component';
+import { AdminHomePageComponent } from './Admin/admin-home-page/admin-home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { MakeRequestComponent } from './User/make-request/make-request.component';
+import { ManageRequestComponent } from './Admin/view-requests/manage-request.component';
+import { ManageRequestButtonsComponent } from './Admin/manage-request-buttons/manage-request-buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerHomePageComponent,
     AdminHomePageComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    MakeRequestComponent,
+    ManageRequestComponent,
+    ManageRequestButtonsComponent
   ],
   imports: [
     BrowserModule,
