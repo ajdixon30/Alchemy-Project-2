4511560b9f3d019985f6a395b7f3837be0fbe825
BrittLowell <brittany.lowell@revature.net>
2021-11-08 19:57:29 -0700

    added components to front end

--- FrontEnd/CineFile/src/app/app.module.ts
+++ FrontEnd/CineFile/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerHomePageComponent } from './customer-home-page/customer-home-page.component';
+import { AdminHomePageComponent } from './admin-home-page/admin-home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerHomePageComponent,
+    AdminHomePageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
