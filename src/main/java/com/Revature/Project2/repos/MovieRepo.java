61ab83fe28764f8649120f193f11e4c28844df95
Hawkx1 <gregg.friedman@revature.net>
2021-11-10 18:10:46 -0500

    Reversing changes

--- src/main/java/com/Revature/Project2/repos/MovieRepo.java
+++ src/main/java/com/Revature/Project2/repos/MovieRepo.java
@@ -11,8 +11,8 @@ public interface MovieRepo extends JpaRepository<Movie, Integer> {
     @Query(value = "SELECT * FROM movie WHERE title = ?1", nativeQuery = true)
     public String movieSearch(String title);
 
-    //List<Movie> findByGenre(String genre);
+    List<Movie> findByGenre(String genre);
 
-    @Query(value = "SELECT title FROM movie WHERE genre = ?1", nativeQuery = true)
+    @Query(value = "SELECT name FROM movie WHERE genre = ?1", nativeQuery = true)
     public List<String> filterGenre(String value);
 }
