d46d9ae5fb43f3f999eecd7097f06e8537429ef8
BrittLowell <brittany.lowell@revature.net>
2021-11-11 09:30:14 -0700

    fixed errors on front end. Adding another section to the navbar for the links that change based on login status

--- FrontEnd/CineFile/src/app/app-routing.module.ts
+++ FrontEnd/CineFile/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { CustomerSignupComponent } from './customer-signup/customer-signup.compo
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewMoviesComponent } from './User/view-movies/view-movies.component';
+import { MakeRequestComponent } from './User/make-request/make-request.component';
+import { RateMovieComponent } from './User/rate-movie/rate-movie.component';
 
 const routes: Routes = [
   { path: "admin-login", component: AdminLoginComponent },
   { path: "customer-signup", component: CustomerSignupComponent},
-  { path: "customer-login", component: LoginPageComponent }
+  { path: "customer-login", component: LoginPageComponent },
+  { path: "view-movies", component: ViewMoviesComponent},
+  { path: "make-request", component: MakeRequestComponent},
+  { path: "rate-movie", component: RateMovieComponent}
 ];
 
 @NgModule({
