20cc31628c6349c981054f73739a543cb30a577a
James Brown <42588084+brownje3@users.noreply.github.com>
2021-11-11 23:24:54 -0500

    RegisterTest possibly working

--- src/test/java/com/Revature/Project2/services/RegisterTest.java
+++ src/test/java/com/Revature/Project2/services/RegisterTest.java
@@ -1,32 +1,42 @@
 package com.Revature.Project2.services;
 
 import com.Revature.Project2.beans.pojos.User;
+import com.Revature.Project2.repos.UserRepo;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ContextConfiguration;
 
 import java.util.ArrayList;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @RunWith(MockitoJUnitRunner.class)
-@ContextConfiguration
+@SpringBootTest
 public class RegisterTest {
 
-    @Mock
+    @InjectMocks
     private Register register;
+
+    @Mock
+    private Validation v;
+
+    @Mock
+    private UserRepo userRepo;
 
     @Test
     public void testIfUserRegistered(){
         User u = new User("user", "pass", "James",
                 "James", false, new ArrayList<>());
-        when(register.userRegister(u));
+
+        when(v.validateName("James", "James")).thenReturn(true);
+        when(v.validateUserCreds("user", "pass")).thenReturn(true);
+
         Assert.assertEquals(HttpStatus.CREATED, register.userRegister(u));
     }
 
