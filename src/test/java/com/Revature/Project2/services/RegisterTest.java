9d8c5e51a02ca275d63b459ec50f059b615d2fbd
James Brown <42588084+brownje3@users.noreply.github.com>
2021-11-11 23:48:51 -0500

    JUnit 5 Register Test

--- src/test/java/com/Revature/Project2/services/RegisterTest.java
+++ src/test/java/com/Revature/Project2/services/RegisterTest.java
@@ -1,24 +1,14 @@
 package com.Revature.Project2.services;
 
 import com.Revature.Project2.beans.pojos.User;
-import com.Revature.Project2.repos.UserRepo;
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 
-import static org.mockito.Mockito.*;
-
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class RegisterTest {
 
