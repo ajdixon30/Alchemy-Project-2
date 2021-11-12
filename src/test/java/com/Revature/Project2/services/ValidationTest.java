5529cc71343f6da39128cc8c87e1e06804580684
James Brown <42588084+brownje3@users.noreply.github.com>
2021-11-12 00:53:25 -0500

    tweak

--- src/test/java/com/Revature/Project2/services/ValidationTest.java
+++ src/test/java/com/Revature/Project2/services/ValidationTest.java
@@ -3,7 +3,6 @@ package com.Revature.Project2.services;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
