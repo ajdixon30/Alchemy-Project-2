1967722d24354d2de950575810975b29be6ea3b8
James Brown <42588084+brownje3@users.noreply.github.com>
2021-11-11 22:20:39 -0500

    Mockito?

--- src/test/java/com/Revature/Project2/services/RegisterTest.java
+++ src/test/java/com/Revature/Project2/services/RegisterTest.java
@@ -1,23 +1,34 @@
 package com.Revature.Project2.services;
 
-import org.junit.Before;
+import com.Revature.Project2.beans.pojos.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
 
 @RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration
 public class RegisterTest {
 
-    @InjectMocks
+    @Mock
     private Register register;
 
-    @Before
-    public void init(){
-    }
-
     @Test
-    public void constructorTest(){
-
+    public void testIfUserRegistered(){
+        User u = new User("user", "pass", "James",
+                "James", false, new ArrayList<>());
+        when(register.userRegister(u));
+        Assert.assertEquals(HttpStatus.CREATED, register.userRegister(u));
     }
+
+
 }
