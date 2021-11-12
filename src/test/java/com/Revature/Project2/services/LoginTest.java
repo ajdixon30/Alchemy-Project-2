9c1554a107b37570f3529eed9caaf8442f1ecc30
James Brown <42588084+brownje3@users.noreply.github.com>
2021-11-12 00:35:24 -0500

    more test stuff

--- src/test/java/com/Revature/Project2/services/LoginTest.java
+++ src/test/java/com/Revature/Project2/services/LoginTest.java
@@ -1,15 +1,30 @@
 package com.Revature.Project2.services;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class LoginTest {
 
-    @Test
-    public void testLogin(){
+    @Autowired
+    Login login;
 
+    @Test
+    public void testUserLogin(){
+        assertEquals(HttpStatus.OK, login.userLogin("user", "pass"));
     }
+
+    @Test
+    public void testAdminLogin(){
+        assertEquals(HttpStatus.OK, login.adminLogin("use", "pass"));
+    }
+
+    @Test
+    public void testIfUserCantAdminLogin(){assertEquals(HttpStatus.UNAUTHORIZED,
+            login.adminLogin("user", "pass"));}
+
 }
