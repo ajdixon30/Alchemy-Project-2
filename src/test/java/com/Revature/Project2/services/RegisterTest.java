0bc5dac993cb69a73f989ee3ef35cf29a946c430
James Brown <42588084+brownje3@users.noreply.github.com>
2021-11-11 19:16:40 -0500

    moved main test file and created registerTest

---
+++ src/test/java/com/Revature/Project2/services/RegisterTest.java
@@ -0,0 +1,24 @@
+package com.Revature.Project2.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RegisterTest {
+
+    @InjectMocks
+    private Register register;
+
+    @Before
+    public void init(){
+    }
+
+    @Test
+    public void constructorTest(){
+
+    }
+}
