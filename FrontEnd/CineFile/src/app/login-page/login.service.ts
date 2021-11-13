7915b78734675cf21e104271351bde2ec55c6cb4
Travis Hood <thood023@gmail.com>
2021-11-13 00:20:02 -0500

    login service in progress

---
+++ FrontEnd/CineFile/src/app/login-page/login.service.ts
@@ -0,0 +1,41 @@
+import { Login } from './../DTOs/login';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LoginService {
+
+  baseUrl = 'http://localhost:8080/request';
+
+  constructor(private client: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  errorHandler(error: any) {
+    let message = '';
+    // let warningLevel = 3;
+    if(error.error instanceof ErrorEvent) {
+        //Get client-side error
+        message = error.error.message;
+    } else {
+        //Get server-side error
+        message = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+
+    //Save the error message here//
+
+    console.log(message);
+    return throwError(() => new Error(message));
+  }
+
+}
