5fdd7c0824d470f9b790f1c3a82102548edb98e4
BrittLowell <brittany.lowell@revature.net>
2021-11-13 12:01:34 -0700

    Moved viewrequest and manage request components into the homescreen

---
+++ FrontEnd/CineFile/src/app/Admin/mangage-requests/mangage-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mangage-requests',
+  templateUrl: './mangage-requests.component.html',
+  styleUrls: ['./mangage-requests.component.css']
+})
+export class MangageRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
