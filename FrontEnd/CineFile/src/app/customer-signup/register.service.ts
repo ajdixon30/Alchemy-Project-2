05b8e2641f17285eb84084b16b3b4212b86c92d3
Travis Hood <thood023@gmail.com>
2021-11-13 12:44:35 -0500

    Register in progress

--- FrontEnd/CineFile/src/app/customer-signup/register.service.ts
+++ FrontEnd/CineFile/src/app/customer-signup/register.service.ts
@@ -1,9 +1,49 @@
+import { Register } from './../DTOs/register';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class RegisterService {
 
-  constructor() { }
+  baseUrl = 'http://localhost:8080/register';//not sure if this is the right thing to have here for the service
+
+  constructor(private client: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+  //get
+  getUser(username: string, password: string, firstName: string, lastName: string): Observable<Register> {
+    return this.client.get<Register>(this.baseUrl + username + password + firstName + lastName)
+    .pipe(
+      retry(0),
+      catchError(this.errorHandler)
+    )
+  }
+
+  errorHandler(error: any) {
+    let message = '';
+    // let warningLevel = 3;
+    if(error.error instanceof ErrorEvent) {
+        //Get client-side error
+        message = error.error.message;
+    } else {
+        //Get server-side error
+        message = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+
+    //Save the error message here//
+
+    console.log(message);
+    return throwError(() => new Error(message));
+  }
+
 }
