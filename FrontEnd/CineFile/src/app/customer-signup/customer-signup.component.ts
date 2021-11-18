2b42a73789d5e27f4467e36465b2cdd9f6d39775
Travis Hood <thood023@gmail.com>
2021-11-18 08:54:34 -0500

    final touch

--- FrontEnd/CineFile/src/app/customer-signup/customer-signup.component.ts
+++ FrontEnd/CineFile/src/app/customer-signup/customer-signup.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { LoginService } from './../Services/login.service';
 import { Component, OnInit } from '@angular/core';
 import { RegisterService } from '../Services/register.service';
 
